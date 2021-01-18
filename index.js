834e0112fcc7cdd02b6d66c50e545ea566914a7b
Pedro Costa <pedrorebelocosta@gmail.com>
2021-01-18 16:26:25 +0000

    stocks and quotations

--- index.js
+++ index.js
@@ -7,12 +7,6 @@ const moment = require('moment');
 
 const app = express();
 const PORT = 3000;
-//Wildcards
-// Test subscription
-// const user = '244121';
-// const subscription = '244121-0001';
-// const appname = 'TESTE2344';
-// const secret = 'b23d3e6d-0ae6-41e0-bd63-5e0a99ae75cb';
 
 //Subscription
 const user = '246568';
@@ -41,39 +35,6 @@ app.get("/", function (req, res) {
 		message: "Sucess"
 	})
 })
-
-//GET KEY
-app.get('/chave', function (req, res) {
-	request({
-		url: 'https://identity.primaverabss.com/core/connect/token',
-		method: 'POST',
-		auth: {
-			user: appname,
-			pass: secret
-		},
-		form: {
-			'grant_type': 'client_credentials',
-			'scope': 'application',
-		}
-	}, function (err, response) {
-		if (response) {
-			let json = JSON.parse(response.body);
-			res.status(200).json({
-				status: true,
-				message: json.access_token
-			});
-			//console.log("Access Token:"+'\n', json.access_token);
-		}
-		else {
-			let json = JSON.parse(response.body);
-			res.status(401).json({
-				status: false,
-				message: 'Something went wrong'
-			});
-			console.log("Could not obtain access token.");
-		}
-	});
-});
 
 app.get('/pickupAvailability', (req, res) => {
 	if (dateString = req.query.datetime) {
@@ -107,35 +68,17 @@ app.get('/pickupAvailability', (req, res) => {
 	}
 });
 
-async function getCalendarData(startStamp, finishStamp) {
-	// GCal expects parameters to be formatted per RFC3339 date norm
-	const calendar = google.calendar({ version: 'v3', auth: getOAuth2Client() });
-
-	const queryResponse = await calendar.freebusy.query({
-		requestBody: {
-			"items": [{ "id" : GOOGLE_REPAIRS_CALENDAR }],
-			"timeMin": startStamp,
-			"timeMax": finishStamp
-		}
-	});
-
-	return queryResponse.data;
-}
-//verificar nif de cliente
-//no postman colocar nif como parametro no body e na value o seu respectivo nif
-//nif - 545192323
-app.post("/verificarnif", function (requesto, resposta) {
-	if (typeof requesto.body.nif === "undefined") {
-		resposta.status(200).json({
+app.get("/get_stock", function (requesto, resposta) {
+	if (typeof requesto.body.itemKey === "undefined") {
+		resposta.status(400).json({
 			status: false,
-			message: "Tens de inserir um nif"
+			message: "itemKey inválido"
 		});
 		return;
 	}
+	var itemKey = requesto.body.itemKey;
 
-	var nif = requesto.body.nif;
-
-	//Pedir um acces token   
+	//Pedir um acces token
 	request({
 		url: 'https://identity.primaverabss.com/core/connect/token',
 		method: 'POST',
@@ -149,12 +92,11 @@ app.post("/verificarnif", function (requesto, resposta) {
 		}
 	}, function (err, res) {
 		if (res) {
-
 			var json = JSON.parse(res.body);
 			var access_token = json.access_token;
+			//var url = `http://my.jasminsoftware.com/api/`+ user +`/`+ subscription +`/materialscore/materialsItems/`+itemKey+`/extension/odata?$filter=itemKey eq '${itemKey}'`;
+			var url = `http://my.jasminsoftware.com/api/` + user + `/` + subscription + `/materialscore/materialsItems/` + itemKey + `/extension/`;
 
-			var url = `http://my.jasminsoftware.com/api/` + user + `/` + subscription + `/salescore/customerParties/odata?$filter=CompanyTaxID eq '${nif}'`;
-			console.log(url);
 			request({
 				url: url,
 				method: "GET",
@@ -167,33 +109,40 @@ app.post("/verificarnif", function (requesto, resposta) {
 				}
 			},
 				function (err, res) {
-
 					if (err) {
-						resposta.status(200).json({
+						resposta.status(400).json({
 							status: false,
-							message: "Tens de inserir um nif"
+							message: "Inserir itemKey"
 						});
 						return;
 					}
-
+					console.log(url);
 					var json = JSON.parse(res.body);
-
-					if (json.items.length > 0) {
+					var i = 1;
+					var wharehouse = json.materialsItemWarehouses
+					var prodstock
+					for (i = 0; i < 3; i++) {
+						if (wharehouse[i].stockBalance > 0) {
+							prodstock = wharehouse[i].stockBalance
+							//console.log(wharehouse[i].stockBalance)
+							console.log(prodstock);
+						}
+					}
+					if (json) {
 						console.log("Sucesso");
 						resposta.status(200).json({
 							status: true,
-							message: json.items[0].name
+							message: "Stock por armazém: " + prodstock
 						});
 					} else {
-						resposta.status(200).json({
+						resposta.status(404).json({
 							status: false,
-							message: "Não existe esse utente"
+							message: "Inexistente"
 						});
 					}
-
 				});
 		} else {
-			resposta.status(200).json({
+			resposta.status(400).json({
 				status: false,
 				message: "Ocorreu um erro ao fazer o pedido de autenticação"
 			});
@@ -202,434 +151,47 @@ app.post("/verificarnif", function (requesto, resposta) {
 	});
 })
 
-
-//pedido para ver clientes (GET)
-//https://my.jasminsoftware.com/245098/245098-0001/#/salescore/customerParties/list?listname=CustomerParties
-app.get("/customers", function (req, res) {
-	request({
-		url: 'https://identity.primaverabss.com/core/connect/token',
-		method: 'POST',
-		auth: {
-			user: appname,
-			pass: secret
-		},
-		form: {
-			'grant_type': 'client_credentials',
-			'scope': 'application',
-		}
-	}, function (err, response) {
-		if (response) {
-			let json = JSON.parse(response.body);
-			let access_token = json.access_token;
-			let url = 'http://my.jasminsoftware.com/api/' + user + '/' + subscription + '/salescore/customerParties';
-
-			request({
-				url: url,
-				method: "GET",
-				headers: {
-					"Authorization": `bearer ${access_token}`,
-					'Content-Type': 'application/json'
-				},
-				form: {
-					scope: 'application'
-				}
-			},
-				function (err, response) {
-					if (err) {
-						response.status(200).json({
-							status: false,
-							message: "Problemas!"
-						});
-						return;
-					}
-
-					let json = JSON.parse(response.body);
-
-					//ver o output do pedido
-					//console.log(json);
-
-					if (json) {
-						res.status(200).json({
-							status: true,
-							message: json
-						});
-					} else {
-						res.status(200).json({
-							status: false,
-							message: "Não existem especialidades!"
-						});
-					}
-				});
-		} else {
-			res.status(200).json({
-				status: false,
-				message: "Ocorreu um erro ao fazer o pedido de autenticação"
-			});
-		}
-	});
-})
-
-//pedido para ver items (GET)
-app.get("/allitems", function (req, res) {
-	request({
-		url: 'https://identity.primaverabss.com/core/connect/token',
-		method: 'POST',
-		auth: {
-			user: appname,
-			pass: secret
-		},
-		form: {
-			'grant_type': 'client_credentials',
-			'scope': 'application',
-		}
-	}, function (err, response) {
-		if (response) {
-			let json = JSON.parse(response.body);
-			let access_token = json.access_token;
-			let url = 'http://my.jasminsoftware.com/api/' + user + '/' + subscription + '/salescore/salesitems';
-
-			request({
-				url: url,
-				method: "GET",
-				headers: {
-					"Authorization": `bearer ${access_token}`,
-					'Content-Type': 'application/json'
-				},
-				form: {
-					scope: 'application'
-				}
-			},
-				function (err, response) {
-					if (err) {
-						response.status(200).json({
-							status: false,
-							message: "Problemas!"
-						});
-						return;
-					}
-					let json = JSON.parse(response.body);
-					//ver o output do pedido
-					//console.log(json);
-
-					if (json) {
-						res.status(200).json({
-							status: true,
-							message: json
-						});
-					} else {
-						res.status(200).json({
-							status: false,
-							message: "Não existem especialidades!"
-						});
-					}
-				});
-		} else {
-			res.status(200).json({
-				status: false,
-				message: "Ocorreu um erro ao fazer o pedido de autenticação"
-			});
-		}
-	});
-})
-
-//api/{tenantKey}/{orgKey}/materialsCore/materialsItems/{itemKey}/extension
-//materialscore/materialsItems/list?listname=MaterialsItems
-
-//TODO: filtrar a resposta json com o resultado e booleano
-app.get("/materials", function (req, res) {
-
-	//itemKey do material
-	var itemKey = req.body.itemKey;
-
-	request({
-		url: 'https://identity.primaverabss.com/core/connect/token',
-		method: 'POST',
-		auth: {
-			user: appname,
-			pass: secret
-		},
-		form: {
-			'grant_type': 'client_credentials',
-			'scope': 'application',
-		}
-	}, function (err, response) {
-		if (response) {
-			let json = JSON.parse(response.body);
-			let access_token = json.access_token;
-			let url = 'http://my.jasminsoftware.com/api/' + user + '/' + subscription + '/materialscore/materialsItems/' + itemKey + '/extension';
-			//let url = 'http://my.jasminsoftware.com/api/'+ user +'/'+ subscription +'/materialscore/materialsItems/';
-
-			console.log(url);
-
-			var data = response;
-
-			var res = data.map(({ status, stockBalance }) => ({ status, stockBalance }));
-			console.log(res);
-
-			request({
-				url: url,
-				method: "GET",
-				headers: {
-					"Authorization": `bearer ${access_token}`,
-					'Content-Type': 'application/json'
-				},
-				form: {
-					scope: 'application'
-				}
-			},
-				function (err, response) {
-					if (err) {
-						response.status(200).json({
-							status: false,
-							message: "Problemas!"
-						});
-						return;
-					}
-					let json = JSON.parse(response.body);
-
-					if (json) {
-						res.status(200).json({
-							status: true,
-							message: json
-						});
-					} else {
-						res.status(200).json({
-							status: false,
-							message: "Não existem dados!"
-						});
-					}
-				});
-		} else {
-			res.status(200).json({
-				status: false,
-				message: "Ocorreu um erro ao fazer o pedido de autenticação"
-			});
-		}
-	});
-})
-
-
-
-
-
-
-
-
-/**
- * Method Type : POST
- * Endpoint : /verificarnif
- * PARAMS:
- *      nif - o número de contribuinte do utente
- * Result:
- *      status - booleano que indica se foi feito o pedido com sucesso
- *      message - string que contêm o nome ou o erro
- */
-app.post("/verificarnif", function (request, resposta) {
-	if (typeof request.body.nif === "undefined") {
-		resposta.status(200).json({
+//Criar orcamento
+//api/{tenantKey}/{orgKey}/sales/quotations/{companyKey}/{documentType}/{serie}/{seriesNumber}/documentLines
+//api/{tenantKey}/{orgKey}/sales/quotations/{companyKey}/{documentType}/{serie}/{seriesNumber}/documentLines/{lineId}/salesItem
+app.post("/create_budget", function (requesto, resposta) {
+	// Validacoes nos pedidos
+	if (typeof requesto.body.salesItem === "undefined") {
+		resposta.status(400).json({
 			status: false,
-			message: "Tem de inserir um nif"
+			message: "salesItem inválido: " + requesto.body.salesItem
+		});
+		return;
+	}
+	if (typeof requesto.body.buyerCustomerParty === "undefined") {
+		resposta.status(400).json({
+			status: false,
+			message: "buyerCustomerParty inválido: " + requesto.body.buyerCustomerParty
+		});
+		return;
+	}
+	if (typeof requesto.body.emailTo === "undefined") {
+		resposta.status(400).json({
+			status: false,
+			message: "emailTo inválido: " + requesto.body.emailTo
 		});
 		return;
 	}
 
-	let nif = request.body.nif;
+	var salesItem = requesto.body.salesItem;
+	var buyerCustomerParty = requesto.body.buyerCustomerParty;
+	var emailTo = requesto.body.emailTo;
+	//data e converter para rfc3339
+	var dateTime = new Date();
+	var dateTimeFormatted = dateTime.toISOString();
 
-	//Pedir um acces token   
-	request({
-		url: 'https://identity.primaverabss.com/core/connect/token',
-		method: 'POST',
-		auth: {
-			user: 'TESTE2344',
-			pass: 'b23d3e6d-0ae6-41e0-bd63-5e0a99ae75cb'
-		},
-		form: {
-			'grant_type': 'client_credentials',
-			'scope': 'application',
-		}
-	}, function (err, res) {
-		if (res) {
-
-			let json = JSON.parse(res.body);
-			let access_token = json.access_token;
-			let url = `http://my.jasminsoftware.com/api/243221/243221-0001/salescore/customerParties/odata?$filter=CompanyTaxID eq '${nif}'`;
-			request({
-				url: url,
-				method: "GET",
-				headers: {
-					"Authorization": `bearer ${access_token}`,
-					'Content-Type': 'application/json'
-				},
-				form: {
-					scope: 'application'
-				}
-			},
-
-				function (err, res) {
-					if (err) {
-						resposta.status(200).json({
-							status: false,
-							message: "Tens de inserir um nif"
-						});
-						return;
-					}
-					let json = JSON.parse(res.body);
-
-					if (json.items.length > 0) {
-						console.log("Sucesso");
-						resposta.status(200).json({
-							status: true,
-							message: json.items[0].name
-						});
-					} else {
-						resposta.status(200).json({
-							status: false,
-							message: "Não existe esse utente"
-						});
-					}
-				});
-		} else {
-			resposta.status(200).json({
-				status: false,
-				message: "Ocorreu um erro ao fazer o pedido de autenticação"
-			});
-		}
-	});
-});
-
-//POST CUSTOMER M
-/**
- *  Method: POST
- *  endpoint: /createcliente
- *  PARAMS:
- *      Nome Próprio 
- *      Data de Nascimento
- *      E-mail 
- *      Telefone
- *      nif
- * 
- *  retorno:
- *      status,
- *      message: id
- */
-app.post("/createcliente", function (request, resposta) {
-
-	if (typeof request.body.nome === "undefined") {
-		resposta.status(200).json({
-			status: false,
-			message: "É necessário inserir um nome"
-		});
-		return;
-	}
-
-	if (typeof request.body.data === "undefined") {
-		resposta.status(200).json({
-			status: false,
-			message: "É necessário inserir uma data"
-		})
-		return;
-	}
-
-	if (typeof request.body.email === "undefined") {
-		resposta.status(200).json({
-			status: false,
-			message: "É necessário inserir um endereço de email"
-		})
-		return;
-	}
-
-	if (typeof request.body.telefone === "undefined") {
-		resposta.status(200).json({
-			status: false,
-			message: "É necessário inserir um número telefónico"
-		})
-		return;
-	}
-
-	if (typeof request.body.nif === "undefined") {
-		resposta.status(200).json({
-			status: false,
-			message: "É necessário inserir um NIF"
-		})
-		return;
-	}
-
-	request({
-		url: 'https://identity.primaverabss.com/core/connect/token',
-		method: 'POST',
-		auth: {
-			user: 'TESTE2344',
-			pass: 'b23d3e6d-0ae6-41e0-bd63-5e0a99ae75cb'
-		},
-		form: {
-			'grant_type': 'client_credentials',
-			'scope': 'application',
-		}
-	}, function (err, res) {
-		if (res) {
-			//fazer o pedido de criaçao de cliente
-			let baseUrlCriarCliente = "http://my.jasminsoftware.com/api/243221/243221-0001";
-			let resourceCriarUtente = "/salesCore/customerParties";
-			let json = JSON.parse(res.body);
-			let access_token = json.access_token;
-			console.log(access_token);
-			let urlPedido = `${baseUrlCriarCliente + resourceCriarUtente}`;
-
-			request({
-				url: urlPedido,
-				method: "POST",
-				headers: {
-					"Authorization": `bearer ${access_token}`,
-					'Content-Type': 'application/json'
-				},
-				form: {
-					scope: 'application'
-				},
-				data: {
-					name: request.body.nome,
-					isExternallyManaged: false,
-					currency: "EUR",
-					isPerson: true,
-					country: "PT"
-				}
-			}, function (err) {
-				if (err) {
-					resposta.status(200).json({
-						status: false,
-						message: "Ocorreu um erro ao fazer o pedido de autenticação"
-					});
-				} else {
-					resposta.status(200).json({
-						status: true,
-						message: "Cliente inserido"
-					});
-				}
-			});
-		} else {
-			resposta.status(200).json({
-				status: false,
-				message: "Ocorreu um erro ao fazer o pedido de autenticação"
-			});
-		}
-	});
-});
-
-//GET ESPECIALIDADE M
-/**
- * METHOD: GET
- * ENDPOINT: /getespecialidades
- * Filtrar por grupo
- * retorna uma lista com as especialidades
- * 
- */
-app.get("/getespecialidades", function (request, resposta) {
 	//Pedir um acces token
 	request({
 		url: 'https://identity.primaverabss.com/core/connect/token',
 		method: 'POST',
 		auth: {
-			user: 'TESTE2344',
-			pass: 'b23d3e6d-0ae6-41e0-bd63-5e0a99ae75cb'
+			user: appname, // TODO : put your application client id here
+			pass: secret // TODO : put your application client secret here
 		},
 		form: {
 			'grant_type': 'client_credentials',
@@ -637,73 +199,73 @@ app.get("/getespecialidades", function (request, resposta) {
 		}
 	}, function (err, res) {
 		if (res) {
-
-			let json = JSON.parse(res.body);
-			let access_token = json.access_token;
-
-			let url = `http://my.jasminsoftware.com/api/243221/243221-0001/salescore/salesitems/odata?$filter=Description eq 'Consulta'`;
-
+			var json = JSON.parse(res.body);
+			var access_token = json.access_token;
+			var url = `https://my.jasminsoftware.com/api/` + user + `/` + subscription + `/sales/quotations`;
+			// var url = `https://my.jasminsoftware.com/api/` + user + `/` + subscription + `/sales/quotations/` + companyKey + `/` + documentType + `/` + serie + `/` + seriesNumber + `/documentLines/`;
+			var body = {
+				"company": "REPAIRMASTERS",
+				"emailTo": emailTo,
+				"buyerCustomerParty": buyerCustomerParty,
+				"documentDate": dateTimeFormatted,
+				"documentLines": [
+					{ "salesItem": salesItem }]
+			};
 			request({
 				url: url,
-				method: "GET",
+				method: "POST",
 				headers: {
 					"Authorization": `bearer ${access_token}`,
 					'Content-Type': 'application/json'
 				},
-				form: {
-					scope: 'application'
-				}
+				json: true,
+				body: body
 			},
-				function (err, res) {
-
+				function (err, res, body) {
 					if (err) {
-						resposta.status(200).json({
+						resposta.status(400).json({
 							status: false,
-							message: "Tenho problemas!"
+							message: "Dados inválidos"
 						});
 						return;
 					}
 
-					let json = JSON.parse(res.body);
-
-					if (json.items.length > 0) {
-						let Arr = [];
-						json.items.forEach(item => Arr.push(item.complementaryDescription));
-						console.log("Sucesso");
-						resposta.status(200).json({
+					if (body) {
+						resposta.status(201).json({
 							status: true,
-							message: Arr
+							message: body
 						});
 					} else {
-						resposta.status(200).json({
+						resposta.status(400).json({
 							status: false,
-							message: "Não existem especialidades!"
+							message: "Bad Request"
 						});
 					}
-
 				});
 		} else {
-			resposta.status(200).json({
+			resposta.status(400).json({
 				status: false,
 				message: "Ocorreu um erro ao fazer o pedido de autenticação"
 			});
+			return;
 		}
 	});
-});
+})
 
-//POST ENCOMENDA 
-/**
- * METHOD: POST
- * ENDPOINT: /createencomenda
- *  PARAMS: idUtente, idEspecialidade/artigo
- * 
- *  status: true
- *  status: 
- * 
- */
-app.post("/createencomenda", function (request) {
+async function getCalendarData(startStamp, finishStamp) {
+	// GCal expects parameters to be formatted per RFC3339 date norm
+	const calendar = google.calendar({ version: 'v3', auth: getOAuth2Client() });
 
-});
+	const queryResponse = await calendar.freebusy.query({
+		requestBody: {
+			"items": [{ "id": GOOGLE_REPAIRS_CALENDAR }],
+			"timeMin": startStamp,
+			"timeMax": finishStamp
+		}
+	});
+
+	return queryResponse.data;
+}
 
 function setOAuth2Client(auth) {
 	googleOAuth2Client = auth;
@@ -760,33 +322,3 @@ app.listen(PORT, function () {
 		authorize(JSON.parse(content), setOAuth2Client);
 	});
 })
-
-
-/*
-let request = require('request');
-
-request({
-  url: 'https://identity.primaverabss.com/core/connect/token',
-  method: 'POST',
-  auth: {
-	user: 'TESTE2344',
-	pass: 'b23d3e6d-0ae6-41e0-bd63-5e0a99ae75cb'
-  },
-  form: {
-	'grant_type': 'client_credentials',
-	'scope': 'application',
-  }
-}, function(err, res) {
-  if (res) {
-	var json = JSON.parse(res.body);
-	console.log("Access Token:", json.access_token);
-
-  }
-  else {
-	console.log("Could not obtain acess token.");
-  }
-});
- */
-
-
-
